c646df3b0a902f00c944f0c6fdf2c42b94c0f091
hnn06 <112699082+hnn06@users.noreply.github.com>
2022-10-05 18:36:22 +0300

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -7,7 +7,7 @@ __global__ void histogram_private_kernel(unsigned char* image, unsigned int* bin
 
     // TODO
     unsigned int i=blockIdx.x*blockDim.x+threadIdx.x;
-    unsigned int j=NUM_BINS-blockDim.x
+    unsigned int j=NUM_BINS-blockDim.x;
     __shared__ unsigned int private_histogram[NUM_BINS];
     if (threadIdx.x+j<NUM_BINS){
         private_histogram[threadIdx.x+j]=0;
